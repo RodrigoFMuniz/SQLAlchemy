b4dbcb35d2d77ddc0a240fe7284feb33413a653e
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-06-16 00:51:54 -0300

    Importing sqlalchemy, Path, sessionmaker, Session, Optional, Engine and ModelBase

---
+++ 03_sql_sync/models/__all_models.py
@@ -0,0 +1,8 @@
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker, Session
+from pathlib import Path
+from typing import Optional
+from sqlalchemy.future.engine import Engine
+
+from model_base import ModelBase
+
