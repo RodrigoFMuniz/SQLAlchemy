2229d65f82f6e173ea9258086202acf736f9c56c
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2022-06-21 20:36:54 -0300

    Created db connection config

--- 03_sql_sync/conf/db_session.py
+++ 03_sql_sync/conf/db_session.py
@@ -1,4 +1,3 @@
-from optparse import Option
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker, Session
 from pathlib import Path
@@ -8,3 +7,16 @@ from sqlalchemy.future.engine import Engine
 from models.model_base import ModelBase
 
 __engine: Optional[Engine] = None
+
+def create_engine(sqlite: bool = True):
+  global __engine
+  if __engine:
+    return
+  if sqlite:
+    arquivo_db = 'db/picole.sqlite'
+    folder = Path(arquivo_db).parent
+    folder.mkdir(parents=True,exist_ok=True)
+    conn_str = f'sqlite:///{arquivo_db}'
+    __engine = sa.create_engine(url=conn_str,echo=False,connect_args ={"check_same_thread":False})
+  else:
+    conn_str = 'postgresql:///user:password@localhost:5432/picoles'
