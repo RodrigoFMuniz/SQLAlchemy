e2b40270593577da8378d06831bb6eee02779436
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-09-30 11:52:25 -0300

    Tabela Revendedor

--- 03_sql_sync/models/revendedor.py
+++ 03_sql_sync/models/revendedor.py
@@ -8,7 +8,7 @@ class Revendedor(ModelBase):
   id:int = sa.Column(sa.BigInteger,primary_key=True, autoincrement=True)
   data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True)
   cnpj:str = sa.Column(sa.String(45), unique=True, nullable=False)
-  razao_social:str = sa.Column(sa.String(100), unique=True, nullable=False)
+  razao_social:str = sa.Column(sa.String(100), nullable=False)
   contato:str = sa.Column(sa.String(100), nullable=False)
 
   def __repr__(self)->str:
