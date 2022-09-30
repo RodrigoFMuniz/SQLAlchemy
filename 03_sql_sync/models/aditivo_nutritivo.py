8e083c725bb363e763b0dc796b0c49a12f9b5794
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-09-30 11:51:23 -0300

    Tabela Revendedor

--- 03_sql_sync/models/aditivo_nutritivo.py
+++ 03_sql_sync/models/aditivo_nutritivo.py
@@ -7,8 +7,8 @@ class AditivoNutritivo(ModelBase):
 
   id:int = sa.Column(sa.BigInteger,primary_key=True, autoincrement=True)
   data_criacao: datetime = sa.Column(sa.DateTime, default=datetime.now, index=True)
-  nome:str = sa.Column(sa.String(45), nullable=False)
-  formula_quimica:str = sa.Column(sa.String(45), nullable=False)
+  nome:str = sa.Column(sa.String(45), unique=True, nullable=False)
+  formula_quimica:str = sa.Column(sa.String(45), unique=True, nullable=False)
 
   def __repr__(self)->str:
     return f"<Aditivo Nutritivo: {self.nome}>"
