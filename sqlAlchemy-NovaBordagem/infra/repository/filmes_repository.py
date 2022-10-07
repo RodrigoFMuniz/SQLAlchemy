b99a76f2dc65e9fb8519a8d5f0e50cbcb294ca73
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-07 01:03:26 -0300

    Efetuado crud completo básico ao banco de dados

--- sqlAlchemy-NovaBordagem/infra/repository/filmes_repository.py
+++ sqlAlchemy-NovaBordagem/infra/repository/filmes_repository.py
@@ -5,4 +5,22 @@ class FilmeRepository:
     def select(self):
         with DBConnectionHandler() as db:
             data = db.session.query(Filme).all()
+            # data = db.get_engine().execute("Select * from filmes").all()
             return data
+    
+    def create(self, filme: Filme):
+        with DBConnectionHandler() as db:
+            db.session.add(filme)
+            db.session.commit()
+    
+    def update(self, filme_id: int):
+        with DBConnectionHandler() as db:
+            db.session.query(Filme).filter(Filme.id == filme_id).update({"ano":2020})
+            db.session.commit()
+    
+    def delete(self, filme_id:int):
+        with DBConnectionHandler() as db:
+            db.session.query(Filme).filter(Filme.id == filme_id).delete()
+            db.session.commit()
+    
+        
--- sqlAlchemy-NovaBordagem/infra/run.py
+++ sqlAlchemy-NovaBordagem/infra/run.py
@@ -1,5 +1,28 @@
 from repository.filmes_repository import FilmeRepository
-
+from entities.filmes import Filme
 repo = FilmeRepository()
-data = repo.select()
-print(data)
+# data = repo.select()
+
+# for d in data:
+#     print(f"ID: {d.id}")
+#     print(f"Título: {d.nome}")
+#     print(f"Tipo: {d.genero}")
+#     print(f"Ano: {d.ano}")
+# print(data[0].nome)
+
+# repo_2 = FilmeRepository()
+# filme_1 = Filme(id=6,nome="Halloween", genero="Terror", ano=2022)
+
+# repo.create(filme_1)
+
+repo.update(6)
+# repo.delete(6)
+
+# repo_3 = FilmeRepository()
+data_2 = repo.select()
+
+for d in data_2:
+    print(f"ID: {d.id}")
+    print(f"Título: {d.nome}")
+    print(f"Tipo: {d.genero}")
+    print(f"Ano: {d.ano}")
