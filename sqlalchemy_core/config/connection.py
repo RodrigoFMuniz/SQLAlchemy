0e98171f6a34ee518552840f1bfc9ed466621ef3
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-21 02:21:09 -0300

    Adicionada uma tabela de cookies para sqlalchemy core

--- sqlalchemy_core/config/connection.py
+++ sqlalchemy_core/config/connection.py
@@ -1,11 +1,31 @@
-from sqlalchemy import create_engine
+from importlib.metadata import metadata
+from sqlalchemy import Column, create_engine, MetaData, Integer, String, Numeric, ForeignKey, Table
 from pathlib import Path
+from datetime import datetime
+
+from sqlalchemy.dialects.sqlite import JSON
+
+metadata = MetaData()
 
 db_name = f"db/cookies.sqlite"
+
 _path = Path(db_name).parent
-# print(Path(conn_str).absolute())
 _path.mkdir(parents=True,exist_ok=True)
+
+
 conn_str = f"sqlite:///{db_name}"
+
+
 engine = create_engine(url=conn_str, echo=True)
 
 connection = engine.connect()
+
+
+cookie = Table('cookies', metadata, 
+    Column('cookie_id', Integer(), primary_key=True),
+    Column('cookie_name', String(50), index=True),
+    Column('cookie_recipe_url', String(255)),
+    Column('cookie_sku', String(55)),
+    Column('quantity', Integer()),
+    Column('unit_cost', Numeric(12,2))
+)
