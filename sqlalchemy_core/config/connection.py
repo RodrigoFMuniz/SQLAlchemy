24ceff0a5dcd2c2a79d765005aef4a6665b25825
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-10-21 02:23:00 -0300

    Adicionado comentários

--- sqlalchemy_core/config/connection.py
+++ sqlalchemy_core/config/connection.py
@@ -12,13 +12,14 @@ db_name = f"db/cookies.sqlite"
 _path = Path(db_name).parent
 _path.mkdir(parents=True,exist_ok=True)
 
-
 conn_str = f"sqlite:///{db_name}"
 
 
 engine = create_engine(url=conn_str, echo=True)
 
 connection = engine.connect()
+
+# teste de inserção de uma tabela
 
 
 cookie = Table('cookies', metadata, 
